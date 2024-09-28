21dc5483c6dfd544049c4f238ab89368e9ac45b0
yudai <yudi0815@icloud.com>
2024-09-29 01:53:54 +0900

    writting quiz

--- word_learning/models.py
+++ word_learning/models.py
@@ -1,8 +1,28 @@
 from django.db import models
+from multiselectfield import MultiSelectField
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class EngWord(models.Model):
+    PART_OF_SPEECH_CHOICES = [
+        ('noun', 'Noun'),
+        ('verb', 'Verb'),
+        ('adjective', 'Adjective'),
+        ('adverb', 'Adverb'),
+        ('pronoun', 'Pronoun'),
+        ('preposition', 'Preposition'),
+        ('conjunction', 'Conjunction'),
+        ('interjection', 'Interjection'),
+        ('article', 'Article'),
+    ]
     eng_word = models.CharField(max_length=30, unique=True)
     meaning = models.CharField(max_length=30, blank=True)
+    part_of_speech = MultiSelectField(max_length=30, 
+                                      choices=PART_OF_SPEECH_CHOICES,
+                                      max_choices=9,
+                                      blank=True)
+    synonyms = models.ManyToManyField('self', blank=True)
+    antonyms = models.ManyToManyField('self', blank=True)
+
 
     def __str__(self):
         return self.eng_word 
@@ -10,4 +30,11 @@ class EngWord(models.Model):
 class ExampleSentence(models.Model):
     sentence = models.CharField(max_length=60)
     eng_word = models.ForeignKey(EngWord, on_delete=models.CASCADE)
+
+class WrittingQuiz(models.Model):
+    quiz = models.CharField(max_length=1000)
+    llm_quiz = models.BooleanField(default=False) #True means created by llm
+    score = models.PositiveIntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(100)])
+    created_at = models.DateTimeField(auto_now_add=True)
+
 
--- word_learning/urls.py
+++ word_learning/urls.py
@@ -5,7 +5,12 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("upload_word", views.upload_word, name="upload_word"),
-    path("word_detail/<str:pk>", views.word_detail, name="word_detail"),
+    path("word_detail/<int:pk>", views.word_detail, name="word_detail"),
     path("word_quiz", views.word_quiz, name="word_quiz"),
     path("result", views.result, name='result'),
+    path("delete_word/<int:word_id>", views.delete_word, name="delete_word"),
+    path("delete_sentence/<int:sentence_id>", views.delete_sentence, name="delete_sentence"),
+    path("revise_detail", views.revise_detail, name="revise_detail"),
+    path("writting_quiz", views.writting_quiz, name="writting_quiz"),
+    path("wr_quiz_page/<int:pk>", views.wr_quiz_page, name="wr_quiz_page"),
 ]
